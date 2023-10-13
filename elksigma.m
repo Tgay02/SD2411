fc1e642e355ab1ce8d8cf0780e706bcb0a660b32
MalatinszkyAdel <malatinszky.adel@gmail.com>
2023-10-13 13:55:30 +0200

    Update elksigma.m

--- elksigma.m
+++ elksigma.m
@@ -1,5 +1,8 @@
 function [Kesigma]=elksigma(le,P,I0,A);
 
+% I0		element polar moment of inertia [m4]
+% A		element cross-section area [m2]
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Assemble element initial stress stiffness matrix
 % File name: elksigma.m
@@ -11,3 +14,11 @@ function [Kesigma]=elksigma(le,P,I0,A);
 % Make sure the initial stress matrix is symmetric!
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+%Given element shape functions
+Nx=[1-3*(x/h)^2+2*(x/h)^3 -x+2*(x^2/h)-(x^3/h) 1-(x-h) 3*(x/h)^2-2*(x/h)^3 (x^2/h)-(x^3/h) x/h]
+%B=dN/dx
+B=diff(Nx,x)
+
+Kesigma= integral(B.T*Nx*B, 0,le)
+end
