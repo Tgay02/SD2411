db59e682cb2e73ed9ff696b71ada795f1a55d6cd
MalatinszkyAdel <malatinszky.adel@gmail.com>
2023-10-14 19:36:32 +0200

    Update elksigma.m

--- elksigma.m
+++ elksigma.m
@@ -1,5 +1,5 @@
-function [Kesigma]=elksigma(le,P,I0,A);
-
+function [Kesigma]=elksigma(le,P,I0,A)
+h=le; %m
 % I0		element polar moment of inertia [m4]
 % A		element cross-section area [m2]
 
@@ -13,12 +13,19 @@ function [Kesigma]=elksigma(le,P,I0,A);
 %
 % Make sure the initial stress matrix is symmetric!
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% x=le;
+% %Given element shape functions
+% Nx=[1-3*(x/h)^2+2*(x/h)^3 -x+2*(x^2/h)-(x^3/h) 1-(x-h) 3*(x/h)^2-2*(x/h)^3 (x^2/h)-(x^3/h) x/h];
+% %B=dN/dx
+% B=diff(Nx,x);
+% 
+% Kesigma= integral(B.T*Nx*B, 0,le);
 
+Kesigma=[36*P/(30*h) -3*h*P/(30*h) 0 -36*P/(30*h) -3*h*P/(30*h) 0;
+    -3*h*P/(30*h) 4*h^2*P/(30*h) 0 3*h*P/(30*h) -h^2*P/(30*h) 0;
+    0 0 I0*P/(A*h) 0 0 -I0*P/(A*h);
+    -36*P/(30*h) 3*h*P/(30*h) 0 36*P/(30*h) 3*h*P/(30*h) 0;
+    -3*h*P/(30*h) -h^2*P/(30*h) 0 3*h*P/(30*h) 4*h^2*P/(30*h) 0;
+    0 0 -I0*P/(A*h) 0 0 I0*P/(A*h)];
 
-%Given element shape functions
-Nx=[1-3*(x/h)^2+2*(x/h)^3 -x+2*(x^2/h)-(x^3/h) 1-(x-h) 3*(x/h)^2-2*(x/h)^3 (x^2/h)-(x^3/h) x/h]
-%B=dN/dx
-B=diff(Nx,x)
-
-Kesigma= integral(B.T*Nx*B, 0,le)
 end
